55ca9b6f76fedbe54b0ed8ce92dcf0931a715351
Ashwin V <ashwinvijikumar@gmail.com>
2026-04-15 16:09:43 +0530

    Theme change, Errors fix and Date and time added

--- gui.py
+++ gui.py
@@ -1,9 +1,12 @@
 import functions
 import PySimpleGUI as gui
+import time
 
+gui.theme('DarkBlack')
+clock = gui.Text(' ', key="clock")
 label = gui.Text("Type in a To-do")
 input_box = gui.InputText(tooltip="Enter To-do", key="todo")
-add_button = gui.Button("Add")
+add_button = gui.Button("Add",size=10)
 list_box = gui.Listbox(values=functions.get_todo(), key="todos",
                        enable_events=True, size=[45,10])
 edit_button = gui.Button("Edit")
@@ -11,17 +14,16 @@ complete_button = gui.Button("Complete")
 exit_button = gui.Button("Exit")
 
 window = gui.Window("To-Do App",
-                    layout=[[label],
+                    layout=[[clock],
+                            [label],
                             [input_box,add_button],
                             [list_box,edit_button,complete_button],
                             [exit_button]],
                     font=('helvetica',20))
 
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
-    print(values['todos'])
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todo()
@@ -29,22 +31,29 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-        case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + '\n'
-
-            todos = functions.get_todo()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-        case "Complete":
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todo()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
             window['todo'].update(value=" ")
+        case "Edit":
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + '\n'
+
+                todos = functions.get_todo()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except:
+                gui.popup("Please select an item first",font=('helvetica',20))
+        case "Complete":
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todo()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value=" ")
+            except:
+                gui.popup("Please select an item first",font=('helvetica',20))
         case "Exit":
             break
         case 'todos':
