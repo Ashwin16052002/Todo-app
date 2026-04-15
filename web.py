c0fbf122c2fddd75145aad9bd1c28690348ea7f1
Ashwin V <ashwinvijikumar@gmail.com>
2026-04-15 22:02:36 +0530

    Implement add and remove todo

--- web.py
+++ web.py
@@ -3,10 +3,21 @@ import functions
 
 todos = functions.get_todo()
 
+def add_todo():
+    todo = web.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
 web.title("Todo App")
 web.write("Start your day with schedule")
 
-for todo in todos:
-    web.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = web.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del web.session_state[todo]
+        web.rerun()
 
-web.text_input(label="",placeholder="Add a new todo...")
+web.text_input(label="",placeholder="Add a new todo...",
+               on_change=add_todo,key="new_todo")
